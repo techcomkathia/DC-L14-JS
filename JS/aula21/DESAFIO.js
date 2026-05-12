e4baeed18c4f9f123807b540a288950461454ace
techcomkathia <techcomk@gmail.com>
2026-05-11 21:43:42 -0300

    atividades com GET em apis públicas

---
+++ JS/aula21/DESAFIO.js
@@ -0,0 +1,10 @@
+// CRIE UMA PÁGINA DE COTAÇÃO DE MOEDAS 
+//https://brasilapi.com.br/docs#tag/CAMBIO/paths/~1cambio~1v1~1moedas/get
+//Usando a api acima faça uma requisição e mostre a conversão do valor inserido pelo usuário em real para dolar, euro e libra esterlina.
+
+
+//faça uma validação de CNPJ: https://brasilapi.com.br/docs#tag/CNPJ
+//consuma a api e mostre as informações do CNPJ inserido pelo usuário. Em caso de erro ou cnpj invalido, mostre uma mensagem de erro. Valide ser foram inseridos a quantidade de digitos corretos. 
+
+//MOSTRE TODOS OS PRODUTOS EM CARDs (use componentes do bootstrap)
+//https://fakestoreapi.com/docs#tag/Products/operation/getAllProducts
