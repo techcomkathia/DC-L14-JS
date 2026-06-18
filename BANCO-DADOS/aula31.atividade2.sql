03bf02514f18be9627b9378feebe6ac438e75bbc
techcomkathia <techcomk@gmail.com>
2026-06-17 21:53:45 -0300

    correção atividade

--- BANCO-DADOS/aula31.atividade2.sql
+++ BANCO-DADOS/aula31.atividade2.sql
@@ -21,5 +21,28 @@ create table if not exists categorias (
     nome varchar(50) not null
 );
 
+/* Tabela livros**
+id -  inteiro
+titulo - textual com até 100 caracteres
+ano_publicacao - inteiro
+preco - decimal com duas casas de precisao
+autor_id - inteiro ( referência autor)
+categoria_id inteiro ( referência categoria)*/
+
+create table if not exists livros (
+	id int auto_increment primary key,
+	titulo varchar(100) not null,
+	ano_publicacao int not null,
+	-- criação das colunas que serão chaves estrangeiras
+	autor_id int not null, -- obrigatória autor/livros (1,n)
+	categoria_id int not null,
+	-- configuração das chaves estrangeiras
+	foreign key (autor_id) references autores (id_autor),
+	foreign key (categoria_id) references categorias (id_categoria)
+);
+
+-- comando extra 
+alter table livros add column preco decimal(10,2);
+
 
 
