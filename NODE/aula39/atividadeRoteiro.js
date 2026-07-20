fcf26c1b3cc7aa2c14e04a12afd0db1eadc215e9
techcomkathia <techcomk@gmail.com>
2026-07-20 19:46:43 -0300

    Base arquivos aula

---
+++ NODE/aula39/atividadeRoteiro.js
@@ -0,0 +1,18 @@
+/* Faça a instalação dos módulos :
+sequelize
+mysql2
+bcrypt
+*/
+
+//npm i sequelize mysql2 bcrypt
+
+/* Crie um banco de dados vazio no MySQL com o nome atividade20_07 
+
+create database atividade20_07;
+use atividade20_07;
+
+*/
+
+/*crie o arquivo servidorBanco e usuariosModel.js*/
+
+//execute o arquivo usuariosModel.js  PARA CRIAR A TABELA DE USUARIOS
---
+++ NODE/aula39/usuariosModel.js
@@ -0,0 +1,33 @@
+//cria uma tabela de usuarios para armazenar os dados dos usuarios cadastrados
+const {DataTypes} = require('sequelize')
+const servidorBanco = require('./servidorBanco')
+
+const Usuarios = servidorBanco.define('usuarios', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        senha: {
+            type: DataTypes.STRING, //não fazemos a deliminação de caracteres, pois a senha vai ser armazenada criptografada
+            allowNull: false
+        }
+    })
+
+module.exports = Usuarios
+
+Usuarios.sync({force: true}).then(() => console.log('Tabela criada com sucesso')).catch((erro) => console.log(erro))
